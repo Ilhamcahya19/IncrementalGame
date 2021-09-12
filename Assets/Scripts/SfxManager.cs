88d893c67a528ccfd8f48d7d48368c920b0bc86f
Ilhamcahya19 <ilhamcahyaaldaffa@gmail.com>
2021-09-12 15:51:35 +0700

    first commit

---
+++ Assets/Scripts/SfxManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SfxManager : MonoBehaviour
+{
+
+    public AudioSource Audio;
+
+    public AudioClip Click,clickUpgrade,clickLocked,congrats;
+
+    public static SfxManager sfxInstance;
+    
+    private void Awake() 
+    {
+        if (sfxInstance != null && sfxInstance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }        
+
+        sfxInstance = this;
+        DontDestroyOnLoad(this);
+    }
+}
---
+++ Assets/Scripts/TapArea.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TapArea : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+
+    public GameObject popclick,among;
+
+    public void OnPointerDown (PointerEventData eventData)
+
+    {
+
+        GameManager.Instance.CollectByTap (eventData.position, transform);
+        popclick.SetActive(true);
+        among.SetActive(false);
+
+    }
+
+
+    public void OnPointerUp (PointerEventData eventData)
+
+    {
+
+        popclick.SetActive(false);
+        among.SetActive(true);
+
+    }
+}
